f99d7b7be136b782a3f8319f6cd52f9690ce54ab
MarcossHT <mppiress@gmail.com>
2022-12-06 16:27:27 -0300

    feat: create routerApp

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./Components/Navbar";
 import Home from "./Routes/Home";
+import Login from "./Routes/Login";
+import Detail from "./Routes/Detail";
 import Footer from "./Components/Footer";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Lembre-se de configurar suas rotas e seu contexto aqui
+const routerApp = createBrowserRouter([
+  {
+    path: '',
+    element: <Home />
+  },
+  {
+    path: 'login',
+    element: <Login />
+  },
+  {
+    path: 'detail',
+    element: <Detail />
+  },
+])
 root.render(
   <React.StrictMode>
     <Navbar />
-    <Home />
+    <RouterProvider router = {routerApp}/>
     <Footer />
   </React.StrictMode>
 );

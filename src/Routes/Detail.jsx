992e798a7a3c6196c9b2392e1bd0f7b1f7bb11d4
MarcossHT <mppiress@gmail.com>
2022-12-09 15:54:58 -0300

    feat: create detail page

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,9 +1,38 @@
 import DetailCard from "../Components/DetailCard";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Detail = () => {
+  
+  const [detail, setDetail] = useState([])
+
+  useEffect(() => {
+    fetch('https://dhodonto.ctdprojetos.com.br/dentista').then(
+      response => {
+        response.json().then(
+          data => {
+            setDetail(data);
+          }
+        )
+      }
+    )
+  }, [])
+  
+  
   return (
     <>
-      <DetailCard />
+    {
+      detail.map(
+        detail => {
+          return (
+            <DetailCard 
+              containerData={detail}
+            />
+          )
+        }
+      )
+    }
+      
     </>
   )
 }

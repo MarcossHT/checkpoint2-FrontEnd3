8e5f669f044c6e2995af6af922d8a39e39030f55
MarcossHT <mppiress@gmail.com>
2022-12-09 15:55:48 -0300

    feat: update DetailCard with api data

--- src/Components/DetailCard.jsx
+++ src/Components/DetailCard.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import ScheduleFormModal from "./ScheduleFormModal";
 import styles from "./DetailCard.module.css";
 
-const DetailCard = () => {
+const DetailCard = (props) => {
 
   useEffect(() => {
     //Nesse useEffect, você vai fazer um fetch na api passando o 
@@ -12,7 +12,7 @@ const DetailCard = () => {
     //As instruções que estão com {''} precisam ser 
     //substituídas com as informações que vem da api
     <>
-      <h1>Detail about Dentist {'Nome do Dentista'} </h1>
+      <h1>Detail about Dentist {props.containerData.nome} </h1>
       <section className="card col-sm-12 col-lg-6 container">
         {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
@@ -28,12 +28,12 @@ const DetailCard = () => {
           </div>
           <div className="col-sm-12 col-lg-6">
             <ul className="list-group">
-              <li className="list-group-item">Nome: {'Nome do Dentista'}</li>
+              <li className="list-group-item">Nome: {props.containerData.nome}</li>
               <li className="list-group-item">
                 Sobrenome: {'Sobrenome do Dentista'}
               </li>
               <li className="list-group-item">
-                Usuário: {'Nome de usuário do Dentista'}
+                Usuário: {props.containerData.sobrenome}
               </li>
             </ul>
             <div className="text-center">
